815ddb5e3cd1065f3e252dc99c84fe4e8ea5a5cb
Madan <madanmohan7272906844@gmail.com>
2022-08-11 23:40:47 +0530

    .

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 let addBtn = document.getElementById('addBtn');
-
 showNotes();
 
 addBtn.addEventListener('click', ()=>{
   let addText = document.getElementById('addText');
+  let addTitle = document.getElementById('addTitle');
+
   let notes = localStorage.getItem('notesID'); // notesID is the key for localStorage
 
   if(notes == null){
     notesObj = []; // creating a array here namesd notesObj
   }else{
     notesObj = JSON.parse(notes); // if the array have some values in it, then it will parse the values and put it in notesObj
+    // it is now array of objects
   }
-  notesObj.push(addText.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addText.value
+  };
+
+  notesObj.push(myObj); // pushing the object in the array
   addText.value = '';
+  addTitle.value = '';
   localStorage.setItem('notesID',JSON.stringify(notesObj)); // when we have to store in local storage, we have to convert it into a string
+  
   showNotes();
 });
 
@@ -30,14 +39,13 @@ function showNotes(){
   notesObj.forEach((element,index)=>{
     // array.forEach(function(currentValue, index){}
     html += `
-
     <div class="notecard my-2 mx-2 card" style="width: 20rem; margin-bottom: 17px;">
       <div class="card-body">
-      <h5 class="card-title">Note ${index+1} </h5>
-        <p class="card-text">${element}</p>
+      <h5 class="card-title">${element.title} </h5>
+        <p class="card-text">${element.text}</p>
         <button id="${index}" onclick="deleteNode(this.id)" class="btn btn-danger">Delete Note</button>
       </div>
-    </div>`
+    </div>`;
   });
   // <h5 class="card-title"><Title>${index+1}</Title></h5>
 
@@ -61,68 +69,9 @@ function deleteNode(index){
 
   let notes = localStorage.getItem('notesID');
 
-  notesObj.splice(index,1); // from the index, delete 1 element (in first iteration index will be 0, so it will delete the first element)
+  notesObj.splice(index,1); // from the index, delete 1 element (like in first iteration index will be 0, so it will delete the first element)
   localStorage.setItem('notesID',JSON.stringify(notesObj));
   showNotes(); // to show the notes after deleting
 }
 
-// localStorage.clear();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let divx = document.getElementById('div1');
-// divx.addEventListener('dblclick',()=>{
-//   // divx.innerText = '';
-//   let textboxx = document.createElement('input');
-//   textboxx.type = 'text';
-//   divx.appendChild(textboxx);
-
-  
-
-//   let addBtn = document.createElement('button');
-//   addBtn.addEventListener('click',()=>{
-//     divx.innerText = textboxx.value;
-//     localStorage.setItem('edited',JSON.stringify(divx.innerText));
-//   });
-//   addBtn.innerText = 'save';
-
-//   divx.appendChild(addBtn);
-//   // divx.innerText = textboxx.value;
-//   // divx.replaceWith(textboxx.value);
-// });
-// // addBtn.addEventListener('click',()=>{
-// //  
-// // });
-// localStorage.clear();
-
+// localStorage.clear(); // to clear the local storage
